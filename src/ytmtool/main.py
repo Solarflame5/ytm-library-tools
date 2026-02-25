5e7db653c497f9f3a09ba9a2da3f32b7d72e3e52
Solarflame <47642904+Solarflame5@users.noreply.github.com>
2026-02-25 21:11:15 +0300

    CLI preparation

--- src/ytmtool/main.py
+++ src/ytmtool/main.py
@@ -1,1 +1,28 @@
 import argparse
+
+
+def handle_list():
+    return
+
+def handle_export():
+    return
+
+def handle_stats():
+    return
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog = "ytmtool"
+    )
+    subparsers = parser.add_subparsers()
+
+    list_parser = subparsers.add_parser("list", help="List playlists in library")
+
+    export_parser = subparsers.add_parser("export", help="Export playlist(s) to JSON file(s)")
+
+    stats_parser = subparsers.add_parser("stats", help="Show statistics from exported playlist")
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
